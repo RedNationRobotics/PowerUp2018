8a52c359d7f727932906bff950d5369c40abbcfb
Rob <robbylohmann@gmail.com>
2018-01-10 11:39:57 -0500

    changes to autonomous chooser

--- src/org/usfirst/frc/team4576/robot/Robot.java
+++ src/org/usfirst/frc/team4576/robot/Robot.java
@@ -1,15 +1,24 @@
+import org.usfirst.frc.team4576.robot.commands.AutoBaseline;
+import org.usfirst.frc.team4576.robot.commands.AutoMiddleGear;
+import org.usfirst.frc.team4576.robot.commands.Autonomous;
+import org.usfirst.frc.team4576.robot.commands.DriveWithJoysticks;
+import org.usfirst.frc.team4576.robot.subsystems.Agitator;
+import org.usfirst.frc.team4576.robot.subsystems.Chassis;
+import org.usfirst.frc.team4576.robot.subsystems.Climber;
+import org.usfirst.frc.team4576.robot.subsystems.MoveAuto;
+import org.usfirst.frc.team4576.robot.subsystems.Pneumatics;
+import org.usfirst.frc.team4576.robot.subsystems.Shooter;
 
-package org.usfirst.frc.team4576.robot;
-
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import org.usfirst.frc.team4576.robot.commands.ExampleCommand;
-import org.usfirst.frc.team4576.robot.subsystems.ExampleSubsystem;
+import redcore.BNO055;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,96 +29,115 @@ import org.usfirst.frc.team4576.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final Chassis chassis = new Chassis();
+	public static final Pneumatics pneumatics = new Pneumatics();
+	private static BNO055 imu;
 	public static OI oi;
 
-	Command autonomousCommand;
-	SendableChooser<Command> chooser = new SendableChooser<>();
+	public static Joystick driveStick = new Joystick(0);
+	public static Joystick secondaryStick = new Joystick(1);
+	UsbCamera camera = new UsbCamera("cam0", 0);
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
+	Command teleopCommand;
+	Command autonomousCommand;
+	final String defaultAuto = "Default";
+	final String baselineAuto = "Baseline Auto";
+	final String timedMiddleGear = "Timed Middle Gear";
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
+	// SendableChooser chooser;
+
 	public void robotInit() {
+		chooser = new SendableChooser<>();
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("Baseline Auto", baselineAuto);
+		chooser.addObject("Middle Gear Auto", timedMiddleGear);
+		SmartDashboard.putData("Auto Choices", chooser);
+		
+		System.out.println("RNR 2017 Robot Code Initializing...");
 		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", chooser);
+		// chooser = new SendableChooser();
+		// chooser.addDefault("Auto Drive 1", new AutoDrive1());
+		// chooser.addObject("Auto Drive 2", new AutoDrive2());
+		// SmartDashboard.putData("Auto Choices:", chooser);
+
+		teleopCommand = new DriveWithJoysticks();
+		autonomousCommand = new Autonomous();
+
+		camera.setFPS(15);
+		camera.setResolution(320, 240);
+		CameraServer.getInstance().startAutomaticCapture(camera);
+		
+		imu = BNO055.getInstance(BNO055.opmode_t.OPERATION_MODE_IMUPLUS,
+		BNO055.vector_type_t.VECTOR_EULER);
+
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
 	public void disabledInit() {
 
 	}
 
-	@Override
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 	}
 
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * This autonomous (along with the chooser code above) shows how to
+	 * select between different autonomous modes using the dashboard. The
+	 * sendable chooser code works with the Java SmartDashboard. If you
+	 * prefer the LabVIEW Dashboard, remove all of the RPMchooser code and
+	 * uncomment the getString code to get the auto name from the text box below
+	 * the Gyro
 	 *
 	 * You can add additional auto modes by adding additional commands to the
 	 * chooser code above (like the commented example) or additional comparisons
 	 * to the switch structure below with additional strings & commands.
 	 */
-	@Override
 	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
 
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
+		switch (chooser.getSelected()) {
+	
+		case "baselineAuto":
+			autonomousCommand = new AutoBaseline();
+			break;
+		case "timedMiddleGear":
+			autonomousCommand = new AutoMiddleGear();
+			break;
+		default:
+			autonomousCommand = new Autonomous();
+			break;
+			
+		}
 
-		// schedule the autonomous command (example)
 		if (autonomousCommand != null)
-			autonomousCommand.start();
+
+	//	chassis.setFPID(0, .2, 0, 0);
+		chassis.initAuto();
+		autonomousCommand.start();
+
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+	//	SmartDashboard.putString("Postions: ", chassis.getPositions());
 	}
 
-	@Override
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
 		if (autonomousCommand != null)
 			autonomousCommand.cancel();
+		chassis.initTeleop();
+		teleopCommand.start();
+		// Robot.chassis.teleopMode();
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
-	}
+		SmartDashboard.putNumber("Left Encoder: ", chassis.tsrxL.getEncPosition());
+		SmartDashboard.putNumber("Right Encoder: ", chassis.tsrxR.getEncPosition());	
+		SmartDashboard.putNumber("BNO055 Heading :", imu.getHeading());
+	//	SmartDashboard.putNumber("BNO055 Heading :", imu.getTemp());
+		}	
 
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
 	public void testPeriodic() {
 		LiveWindow.run();
 	}
