4fd959bd9c8e1ed774eadf143017b939617daf27
Gavin Pretorius <gpretorius.4576@gmail.com>
2018-02-16 17:28:08 -0500

    changed a few things in AutoTurn, SetPoint has an error

--- src/org/usfirst/frc/team4576/robot/commands/AutoTurnAtAngle.java
+++ src/org/usfirst/frc/team4576/robot/commands/AutoTurnAtAngle.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.command.Command;
 
 //*******************************************************************
 //AutoTurnAtAngle				Author: Gavin Pretorius
-//								Last Edited: 2/15/2018 by GP
+//								Last Edited: 2/16/2018 by GP
 //This class is used to turn x amount of degrees during autonomous period. 
 //This will allow us to accurately maneuver during the autonomous period
 //*******************************************************************
@@ -17,12 +17,19 @@ public class AutoTurnAtAngle extends Command {
 	double heading = Robot.imu.getHeading();
 	double _LTurn = heading - 45.0;
 	double _RTurn = heading + 45.0;
-
-	public AutoTurnAtAngle(double setPoint){
-		setPoint would = 45.0;
+	
+	public void AutoTurnAtAngleLeft(double setPoint) {
+		setPoint would = _LTurn;
+		Robot.chassis.tsrxL.set(-.125);
+		Robot.chassis.tsrxR.set(.125);
 	}
-
-
+	
+	public void AutoTurnAtAngleRight(double setPoint) {
+		setPoint would = _LTurn;
+		Robot.chassis.tsrxL.set(.125);
+		Robot.chassis.tsrxR.set(-.125);
+	}
+	
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
