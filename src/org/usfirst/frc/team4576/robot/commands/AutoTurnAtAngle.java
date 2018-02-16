fd5abd8f7fc1a1e4311228409e4e272cf3a70d5c
Gavin Pretorius <gpretorius.4576@gmail.com>
2018-02-15 19:07:54 -0500

    Making Progress for TurnAtAngle

--- src/org/usfirst/frc/team4576/robot/commands/AutoTurnAtAngle.java
+++ src/org/usfirst/frc/team4576/robot/commands/AutoTurnAtAngle.java
@@ -4,30 +4,33 @@ import org.usfirst.frc.team4576.robot.Robot;
 
 //*******************************************************************
 //AutoTurnAtAngle				Author: Gavin Pretorius
-//								Last Edited: 2/14/2018 by GP
+//								Last Edited: 2/15/2018 by GP
 //This class is used to turn x amount of degrees during autonomous period. 
 //This will allow us to accurately maneuver during the autonomous period
 //*******************************************************************
 
 public class AutoTurnAtAngle {
 	 
-	public void Loop() {
-		
-
-		//we can choose to turn left or right
-		double _LTurn = Robot.imu.getHeading() - 45.0;
-		double _RTurn = Robot.imu.getHeading() + 45.0;
+	double heading = Robot.imu.getHeading();
+	
+	public void TurnLeft() {
+				
+		double _LTurn = heading - 45.0;
 		
 		while(_LTurn != Robot.imu.getHeading()) {
 			Robot.chassis.tsrxL.set(-.125);
 			Robot.chassis.tsrxR.set(.125);
 		}
+	}
+	
+	
+	public void TurnRight() {
+		double _RTurn = heading + 45.0;
 		
 		while(_RTurn != Robot.imu.getHeading()) {
 			Robot.chassis.tsrxL.set(.125);
-			Robot.chassis.tsrxR.set(.125);
+			Robot.chassis.tsrxR.set(-.125);
 		}
-	
-		}
+	}
 
 }
