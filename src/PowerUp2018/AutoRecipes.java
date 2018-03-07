ad6ac006ba5d551dc1607555cbb5f0d5f3670dda
Rob <robbylohmann@gmail.com>
2018-03-07 15:22:39 -0500

    fixing stuff that was accidentally removed

--- src/PowerUp2018/AutoRecipes.java
+++ src/PowerUp2018/AutoRecipes.java
@@ -69,16 +69,13 @@ public class AutoRecipes {
 			new MotionItem(EAutoStates.eDriveForward, 5.0),
 			//Move Elevator up to Switch Height
 			new MotionItem(EAutoStates.eSetLiftHeight, 35.0),
-			new MotionItem(EAutoStates.eChained_WaitLift),
 			//Release cube
 			new MotionItem(EAutoStates.eGripper_Release),
 			new MotionItem(EAutoStates.eStartTimer, .4),
 			new MotionItem(EAutoStates.eGripper_Stop),
 			//Back up, Lower Lift, then stop.
 			new MotionItem(EAutoStates.eDriveForward, -5.0),
-			new MotionItem(EAutoStates.eChained_MoveWait), 
 			new MotionItem(EAutoStates.eSetLiftHeight, -30.0),
-			new MotionItem(EAutoStates.eChained_WaitLift),
 			new MotionItem(EAutoStates.eStopMotors),
 			new MotionItem(EAutoStates.eStopElevator),
 			new MotionItem(EAutoStates.eIdle) 
