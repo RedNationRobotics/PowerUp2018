ce7d50296b76ce689c98b2f693e6013f9e2506c1
Gavin Pretorius <gpretorius.4576@gmail.com>
2018-05-23 21:45:01 -0400

    Adding stuff for packets

---
+++ src/Pixyrio/PixyException.java
@@ -0,0 +1,10 @@
+package Pixyrio;
+public class PixyException extends Exception{
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    public PixyException(String message){
+        super(message);
+    }
+}
